c95a790ae89527689c58b2b673f7e7d11d8be4fb
Allison Shaw <shaw.allison91@gmail.com>
2018-03-06 08:47:06 -0700

    need to fix newsies

--- exercises/loops-and-arrays/main.js
+++ exercises/loops-and-arrays/main.js
@@ -11,8 +11,44 @@ var officeItems = ["stapler",
                    "computer", 
                    "computer"];
 
-for (var i = 0; i < officeItems.length; i++){
-  console.log(officeItems[i])
-};
+// function itemSort(arr, string) {
+//   var count = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === string) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 
+// console.log(itemSort(officeItems, 'computer'));
+
+
+////////////////////////////////////////////////
+//Loop through the following array and log to the console "old enough" if they are 18 or older, 
+//and "not old enough" if they aren't 18.
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+    name: "Mike",
+    age: 12,
+    gender: "male"
+  }, {
+    name: "Madeline",
+    age: 80,
+    gender: "female"
+  }, {
+    name: "Cheryl",
+    age: 22,
+    gender: "female"
+  }, {
+    name: "Sam",
+    age: 30,
+    gender: "male"
+  }, {
+    name: "Suzy",
+    age: 4,
+    gender: "female"
+  }
+];

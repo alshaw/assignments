aeaa59b5535f570f29defb4df09f9e13775be3a5
Allison Shaw <shaw.allison91@gmail.com>
2018-03-09 12:41:50 -0700

    string manipulation working

--- exercises/readline-sync-string-manipulation/index.js
+++ exercises/readline-sync-string-manipulation/index.js
@@ -4,8 +4,8 @@ var greeting = rs.question('Hi, hey, hello. How are you today? \n');
 console.log('Oh, I am also doing ' + greeting.toUpperCase() + '.');
 
 // Report back the number of characters in a given input
-var countChar = rs.question('What is your favorite season? \n');
-// console.log(countChar ' is ' + ' characters long. \n');
+var favSeason = rs.question('What is your favorite season? \n');
+console.log("Your favorite season has " + favSeason.length + " letters in it!");
 
 // Concatenate (add) two separate strings together
 var favAnimal = rs.question('What is your favorite animal? \n');
@@ -13,4 +13,22 @@ console.log('You like ' + greeting.concat() + ' ' + favAnimal);
 
 // Given a long message(more than twenty letters), return only the last half of the message
 var funFact = rs.question('Tell me the most fun fact you know. \n');
-var lastHalf = funFact.substr(Math.floor(funFact.length / 2));
+
+var halfFact = function (funFact) {
+  if (funFact.length >= 20) {
+    var shortFact = funFact.length / 2;
+  } else {
+    return funFact;
+  }
+  return funFact.slice(shortFact, funFact.length);
+}
+
+console.log(halfFact(funFact) +  " is only half of your fact");
+
+var location = rs.question("What is favorite number between 0 and " + funFact.length + '\n');
+
+var favNumber = function (funFact, location) {
+  return funFact.slice(location, funFact.length);
+}
+
+console.log(favNumber(funFact, location));

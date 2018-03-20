a8f19aee8798a5a6dd572ee04605edc16baab2ef
Allison Shaw <shaw.allison91@gmail.com>
2018-03-20 12:29:52 -0600

    changed go speed clicker

--- exercises/go-speed-clicker/script.js
+++ exercises/go-speed-clicker/script.js
@@ -1,22 +1,6 @@
-// var button = document.getElementById("button");
-// var countClicks = 0;
-
-// window.addEventListener("click", function (button) {
-//   console.log("I clicked");
-//   countClicks++
-// });
-
-
-// sessionStorage.setItem("click", click);
-// console.log(sessionStorage.getItem("click", click));
-
-function clickCounter() {
-  if(typeof(Storage) !== "undefined") {
-    if(localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickCount)+1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("result").innerHTML = "You have clicked " + localStorage.clickcount + " time(s).";
-  }
+var button = document.getElementById('clickme')
+count = 0
+button.onclick = function () {
+  count += 1
+  button.innerHTML = 'Click me: ' + count
 }

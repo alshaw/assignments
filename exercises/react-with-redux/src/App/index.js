db293c6d10cfa95f23f9918ab8bfdf82b5cf5686
Allison Shaw <shaw.allison91@gmail.com>
2018-04-05 17:43:09 -0600

    react-with-redux double and half working

--- exercises/react-with-redux/src/App/index.js
+++ exercises/react-with-redux/src/App/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../redux';
+import { increment, decrement, double, half } from '../redux';
 
 function App(props) {
   return (
     <div className="app">
+      <button onClick={props.half}>/2</button>
       <button onClick={props.decrement}>-</button>
       <h1>{props.counter}</h1>
       <button onClick={props.increment}>+</button>
+      <button onClick={props.double}>x2</button>
     </div>
   )
 }
@@ -18,7 +20,7 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { increment, decrement })(App);
+export default connect(mapStateToProps, { increment, decrement, double, half })(App);
 
 // Two reasons why you want to connect to the store
 // 1. Does this component need any data from the Redux store.
--- exercises/react-with-redux/src/redux/index.js
+++ exercises/react-with-redux/src/redux/index.js
@@ -12,12 +12,28 @@ export function decrement() {
   }
 }
 
+export function double() {
+  return {
+    type: "DOUBLE"
+  }
+}
+
+export function half() {
+  return {
+    type: "HALF"
+  }
+}
+
 function reducer(state = 0, action) {
   switch(action.type) {
     case "INCREMENT":
       return state + 1;
     case "DECREMENT":
       return state - 1;
+    case "DOUBLE":
+      return state * 2;
+    case "HALF":
+      return state / 2;
     default:
       return state;
   }

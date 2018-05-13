facdfedddfc8434be6e3fdff8e090eae40ccbf41
Allison Shaw <shaw.allison91@gmail.com>
2018-05-12 20:40:49 -0600

    fixed errors

--- projects/rock-the-vote/client/src/App/IssuesList/Issue/index.js
+++ projects/rock-the-vote/client/src/App/IssuesList/Issue/index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Feed, Icon, Button } from "semantic-ui-react";
 import EditForm from "./EditForm";
 import Comment from "./Comment";
-import CommentList from "./Comment/CommentList";
+// import CommentList from "./Comment/CommentList";
 import { deleteIssue, editIssue } from "../../../redux/issues";
 
 class Issue extends Component {
@@ -15,7 +15,6 @@ class Issue extends Component {
     };
     this.toggleEdit = this.toggleEdit.bind(this);
     this.toggleComment = this.toggleComment.bind(this);
-    this.toggleViewComment = this.toggleViewComment.bind(this);
   }
 
   toggleEdit(e) {
@@ -28,12 +27,6 @@ class Issue extends Component {
     this.setState(prevState => ({
       isCommenting: !prevState.isCommenting
     })); 
-  }
-
-  toggleViewComment(e) {
-    this.setState(prevState => ({
-      isViewing: !prevState.isViewing
-    }))
   }
 
   render() {
@@ -59,13 +52,6 @@ class Issue extends Component {
         <Comment {...this.props} toggleComment={this.toggleComment}/>
       </div>
     }
-
-    if(this.state.isViewing) {
-      return <div>
-        <CommentList {...this.props} toggleViewComment={this.toggleViewComment} />
-      </div>
-    }
-
 
 
     return <div>
@@ -99,9 +85,7 @@ class Issue extends Component {
         <button className="submit" onClick={() => deleteIssue(_id)}>
           Delete
         </button>
-        <button onClick={this.toggleViewComment}>
-          View Comments
-        </button>
+
       </div>;
   }
 }
